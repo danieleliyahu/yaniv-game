e69c80798749055ba502361c52487a54088d1067
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-02-25 18:09:02 +0200

    create 52 cards

---
+++ index.js
@@ -0,0 +1,29 @@
+class Card{
+    constructor(suit,rank,isJocker = false,){
+        this.suit = suit
+        this.isJocker = isJocker
+        this.rank = rank
+    }
+}
+// class Deck{
+//     constructor(numberOfCards){
+//      this.numberOfCards = numberOfCards;
+//      create(){
+
+//      }
+//     }
+// }
+function createCard(){
+    const cards={};
+    const shapes = ["Spade", "Heart", "Club", "Diamond"]
+    for(let shape of shapes){
+        for(let i = 1; i<=13; i++){
+            const propName = shape+i
+                cards[propName]=new Card(shape, i,undefined,);
+            
+        }
+
+    }
+    return cards;
+}
+console.log(createCard())
